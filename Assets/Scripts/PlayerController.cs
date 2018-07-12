27982a254350691105545b35d6eb3f4be3e525ce
Bloooo8 <skrzypapiotr144@gmail.com>
2018-07-12 16:20:13 +0200

    Prototyp sterowania i obiektów do kolekcjonowania

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour,ICollector{
+
+    new Rigidbody rigidbody;
+    public bool OnGround { get; private set; }
+    float speed = 0.02f;
+    float forceMultiplier = 100f;
+    Vector3 jumpForce;
+    Vector3 downForce;
+    Vector3 rightForce;
+    Vector3 leftForce;
+    TouchManager touchManager;
+    RunInfo runInfo;
+
+
+    public void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        touchManager = FindObjectOfType<TouchManager>();
+        runInfo = FindObjectOfType<RunInfo>();
+       
+    }
+    public void Update()
+    {
+        InitializeForces();
+        MoveForward();
+        CheckIfOnGround();
+        DoMove();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CheckIfCollectable(other))
+        {
+            Collect(other.GetComponent<ICollectable>());
+        }
+    }
+   
+    void InitializeForces()
+    {
+        jumpForce = new Vector3(0f, forceMultiplier, forceMultiplier / 10f);
+        downForce = new Vector3(0f, -forceMultiplier, forceMultiplier / 10f);
+        rightForce = Vector3.right * forceMultiplier;
+        leftForce = Vector3.left * forceMultiplier;
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * speed);
+    }
+    void CheckIfOnGround()
+    {
+        if (transform.position.y < 0.140)
+            OnGround = true;
+        else
+            OnGround = false;
+    }
+
+    void DoMove()
+    {
+        SWIPE swipe = touchManager.SwipeDirection;
+        if (swipe != SWIPE.NULL && OnGround)
+        {
+            switch (swipe)
+            {
+                case SWIPE.UP:
+                    rigidbody.AddForce(jumpForce, ForceMode.Impulse);
+                    break;
+                case SWIPE.DOWN:
+                    rigidbody.AddForce(downForce, ForceMode.Impulse);
+                    break;
+                case SWIPE.RIGHT:
+                    rigidbody.AddForce(rightForce, ForceMode.Impulse);
+                    break;
+                case SWIPE.LEFT:
+                    rigidbody.AddForce(leftForce, ForceMode.Impulse);
+                    break;
+            }
+        }
+    }
+
+   public void Collect(ICollectable item)
+    {
+        runInfo.InreaseCoinAmount();
+        item.Destroy();
+        Debug.Log("Zebrano monetę, ilość monet: " + runInfo.CoinAmount);
+    }
+
+    bool CheckIfCollectable(Collider collider)
+    {
+        if (IsCollectable(collider.gameObject))
+        {
+            return true;
+        }          
+        else
+        {
+            return false;
+        }          
+    }
+
+    bool IsCollectable(GameObject item)
+    {
+        if (item.GetComponent<ICollectable>() != null)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ Assets/Scripts/RunInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunInfo : MonoBehaviour {
+
+   public int CoinAmount { get; private set;}
+
+    void Start()
+    {
+        CoinAmount = 0;
+    }
+
+    public void InreaseCoinAmount()
+    {
+        CoinAmount++;
+    }
+}
